fb7633cf38c9a6740b08141573de8cb2de0efcbf
Mikel Hinojosa <boras728@hotmail.com>
2024-03-19 14:18:25 -0600

    Pilas&Obj

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       Pila pila= new Pila();
-       pila.push(0);
-       pila.push(4);
-       pila.push(45);
-       pila.push(45);
-       pila.push(120);
-       pila.push(85);
-       pila.push(100);
-       while(!pila.empty()){
-           System.out.println("pila.pop() = " + pila.pop());
-       }
+        PilaObjeto pilaObjeto = new PilaObjeto();
+        pilaObjeto.push("Mikel");
+        pilaObjeto.push(true);
+        pilaObjeto.push(false);
+        pilaObjeto.push(2.2f);
+        pilaObjeto.push(10000000000000f);
+        pilaObjeto.pop();
+        while (!pilaObjeto.empty()){
+            System.out.println("pilaObjeto = " + pilaObjeto.pop());
+        }
     }
 }
